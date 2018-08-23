43bde6ed095aaa6b04ea0eb9023c71397b9e3952
Nicky Liu <nickyliu91@gmail.com>
2018-08-23 11:42:03 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,12 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
+  names_array = []
+  data.each do |attribute, hash|
+    hash.each do |attribute_answer, names|
+      names.each do |name|
+        names_array << name
+      end
+    end
+  end
+  put names_array
 end
